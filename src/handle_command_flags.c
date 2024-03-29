2a197947c1c7c7cbd7a913def64d3c70d591e6be
Aegis-ID <pierre.nguyen@epitech.eu>
2024-03-29 09:14:05 +0100

    [ADD:handle_command_flags] for future flags

---
+++ src/handle_command_flags.c
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2024
+** handle command flags
+** File description:
+** minishell-2
+*/
+
+#include "my.h"
+#include "lib.h"
+
+int handle_command_flags(char *input, char **parsed_input,
+    list parsed_env, list history)
+{
+    int status = 0;
+
+    for (int i = 0; input[i] != '\0'; i++) {
+        if (input[i] == ';') {
+            status = handle_semicolons(input, parsed_input,
+            parsed_env, history);
+        }
+    }
+    return status;
+}
