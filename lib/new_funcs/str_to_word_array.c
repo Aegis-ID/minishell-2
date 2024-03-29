15b65396b25a0b9a04c8a1012a4a931970956ddd
Aegis-ID <pierre.nguyen@epitech.eu>
2024-03-29 09:13:03 +0100

    [FIX:str_to_word_array] fix bad array size algorithm

--- lib/new_funcs/str_to_word_array.c
+++ lib/new_funcs/str_to_word_array.c
@@ -13,9 +13,9 @@ static int get_array_size(char *str, char *delim)
     int size = 0;
 
     for (int i = 0; str[i] != '\0'; i++) {
-        if (!is_in_str(str[i], delim) && (i == 0))
+        if (!is_in_str(str[i], delim) && is_in_str(str[i + 1], delim))
             size++;
-        if (!is_in_str(str[i], delim) && is_in_str(str[i - 1], delim))
+        if (!is_in_str(str[i], delim) && (str[i + 1] == '\0'))
             size++;
     }
     return size;
